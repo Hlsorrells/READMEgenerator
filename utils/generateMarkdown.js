823f0087adc8bb2f5c6cd9ed8ecb162a7081f293
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-05-22 21:25:14 -0400

    created readme template in markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,34 @@ function generateMarkdown(data) {
   return `
 # ${data.title}
 
+## Description
+${data.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+To install, please run the following command in terminal:
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under the ${data.license} license.
+
+## Contributing
+${data.contribution}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions about the repo, open an issue or contact  ${data.username} directly at ${data.email}
 `;
 }
 

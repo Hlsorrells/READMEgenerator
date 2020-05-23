1800d323b695536af49108340c5c44d76f277704
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-05-22 21:21:00 -0400

    created array of questions, and requires for fs and inquirer const

--- index.js
+++ index.js
@@ -1,5 +1,62 @@
-const questions = [
+const fs = require("fs");
+const inquirer = require("inquirer");
 
+const questions = [
+        {
+            type: "input",
+            message: "What is your GitHub username?",
+            name: "username"
+        },
+        {
+            type: "input",
+            message: "What is your email address?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is the URL to your project?",
+            name: "URL"
+        }
+        {
+            type: "input",
+            message: "What is your project title?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "Please provide a short description of the project.",
+            name: "description"
+        },
+        {
+            type: "input",
+            message: "What command should be run to install dependencies?",
+            name: "installCode"
+        },
+        {
+            type: "input",
+            message: "How does the user utilize the repo?",
+            name: "usage"
+        },
+        {
+            type: "list",
+            message: "Is the project protected by a license?",
+            name: "license",
+            choices: [
+                "MIT",
+                "ISC",
+                "UNLICENSED"
+            ]
+        },
+        {
+            type: "input",
+            message: "How can the user make contributions to the repo?",
+            name: "contribution"
+        },
+        {
+            type: "input",
+            message: "What command is needed to run tests?",
+            name: "tests"
+        }
 ];
 
 function writeToFile(fileName, data) {
